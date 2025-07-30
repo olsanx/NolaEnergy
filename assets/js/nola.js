6eb87d11cb52b82b7538ea2affd6a45a7664d1df
olsanx <121573851+olsanx@users.noreply.github.com>
2025-07-30 01:12:26 +0100

    Add files via upload

--- assets/js/nola.js
+++ assets/js/nola.js
@@ -27,19 +27,6 @@ document.querySelectorAll('.side-nav a[href^="#"]').forEach(link => {
 });
 
 
-//smooth scroll
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute('href'));
-      if (target) {
-        target.scrollIntoView({ behavior: 'smooth' });
-      }
-    });
-  });
-
-
-
 
   // scroll up
   window.addEventListener("load", () => {
